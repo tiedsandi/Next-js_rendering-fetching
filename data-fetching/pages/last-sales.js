60af561e06757d3164ccc10d0b9ebb478d6e57da
tiedsandi <fachransandi@gmail.com>
2024-03-25 08:24:46 +0700

    using useSWR

--- data-fetching/pages/last-sales.js
+++ data-fetching/pages/last-sales.js
@@ -1,37 +1,63 @@
 import {useEffect, useState} from 'react';
+import useSWR from 'swr';
 
 function LastSalesPage() {
   const [sales, setSales] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  const {data, error} = useSWR(
+    'https://next-js-course2-a4267-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json',
+    (url) => fetch(url).then((response) => response.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch(
-      'https://next-js-course2-a4267-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json'
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
+    if (data) {
+      const transformedSales = [];
 
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
 
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
+      setSales(transformedSales);
+    }
+  }, [data]);
 
-  if (isLoading) {
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(
+  //     'https://next-js-course2-a4267-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json'
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
   if (!sales || sales.length === 0) {
-    return <p>No data yet</p>;
+    return <p>No sales data yet.</p>;
   }
 
   return (
